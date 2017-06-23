e81fff09bc71965d34d0d8e33f35c6c4842ce667
Junpei Kawamoto <kawamoto.junpei@gmail.com>
2017-06-23 02:42:55 -0400

    Reimplement Display using gocui instead of using termbox-go directly.

--- command/display.go
+++ command/display.go
@@ -19,7 +19,7 @@ import (
 	"strings"
 	"sync"
 
-	termbox "github.com/nsf/termbox-go"
+	"github.com/jroimartin/gocui"
 	"github.com/ttacon/chalk"
 )
 
@@ -31,21 +31,41 @@ type Section struct {
 	display *Display
 }
 
+func newSection(display *Display, header string) *Section {
+
+	return &Section{
+		Header:  header,
+		display: display,
+	}
+
+}
+
 // Writer returns io.WriteCloser to write messages into the section. Users have
 // to close the returned writer.
 func (s *Section) Writer() io.WriteCloser {
 
 	reader, writer := io.Pipe()
-	go func(s *Section) {
+	go func() {
 		defer reader.Close()
+
 		scanner := bufio.NewScanner(reader)
 		for scanner.Scan() {
 			s.Body = append(s.Body, scanner.Text())
-			if err := s.display.Refresh(); err != nil {
+			s.display.gui.Execute(func(g *gocui.Gui) (err error) {
+				v, err := g.View(s.Header)
+				if err != nil {
+					return
+				}
+				v.Clear()
+				for _, line := range s.Body {
+					fmt.Fprintln(v, line)
+				}
 				return
-			}
+			})
 		}
-	}(s)
+
+	}()
+
 	return writer
 
 }
@@ -54,70 +74,89 @@ func (s *Section) Writer() io.WriteCloser {
 func (s *Section) String() string {
 
 	return fmt.Sprintf(
-		"─ %v %v\n%v",
+		"%v\n%v",
 		chalk.Bold.TextStyle(s.Header),
-		strings.Repeat("─", s.display.Width-len(s.Header)-3),
 		strings.Join(s.Body, "\n"))
 
 }
 
 // Display represents a display which consists of several sections.
 type Display struct {
-	HeaderForeground termbox.Attribute
-	HeaderBackground termbox.Attribute
-	BodyForeground   termbox.Attribute
-	BodyBackground   termbox.Attribute
-	Width            int
-	Height           int
-	mutex            sync.Mutex
-	sections         []*Section
-	closed           bool
-	done             chan error
+	mutex    sync.Mutex
+	sections []*Section
+	closed   bool
+	done     chan error
+	gui      *gocui.Gui
 }
 
 // NewDisplay creates a new display. The new display hooks any key inputs
 // including SIGINT, if it receives that signal, the given cancel function
 // will be called.
-func NewDisplay(cencel context.CancelFunc) (display *Display, err error) {
+func NewDisplay(cancel context.CancelFunc) (display *Display, err error) {
 
-	err = termbox.Init()
+	g, err := gocui.NewGui(gocui.OutputNormal)
 	if err != nil {
 		return
 	}
 
-	width, height := termbox.Size()
 	display = &Display{
-		HeaderForeground: termbox.ColorDefault,
-		HeaderBackground: termbox.ColorDefault,
-		BodyForeground:   termbox.ColorDefault,
-		BodyBackground:   termbox.ColorDefault,
-		Width:            width,
-		Height:           height,
-		done:             make(chan error),
+		gui:  g,
+		done: make(chan error),
+	}
+	g.SetManager(display)
+
+	err = g.SetKeybinding("", gocui.KeyCtrlC, gocui.ModNone, func(g *gocui.Gui, v *gocui.View) error {
+		cancel()
+		return gocui.ErrQuit
+	})
+	if err != nil {
+		g.Close()
+		return
 	}
 
 	go func() {
-		for {
-			e := termbox.PollEvent()
-			switch e.Type {
-			case termbox.EventError:
-				display.done <- e.Err
-				return
-			case termbox.EventKey:
-				if e.Key == termbox.KeyCtrlC {
-					cencel()
-					display.done <- fmt.Errorf("Canceled")
-					return
-				}
-			case termbox.EventResize:
-				display.Width = e.Width
-				display.Height = e.Height
-				display.Refresh()
-			}
+		err := g.MainLoop()
+		if err == gocui.ErrQuit {
+			err = nil
 		}
+		display.done <- err
 	}()
 
 	return
+
+}
+
+// Layout is called every time the GUI is redrawn, it must contain the
+// base views and its initializations.
+func (d *Display) Layout(g *gocui.Gui) error {
+	d.mutex.Lock()
+	defer d.mutex.Unlock()
+
+	if len(d.sections) == 0 {
+		return nil
+	}
+	if d.closed {
+		return fmt.Errorf("Display has been closed already")
+	}
+
+	width, height := g.Size()
+	hOffset := height / len(d.sections)
+	// hExtra := height - hOffset*len(d.sections)
+
+	for i, s := range d.sections {
+
+		v, err := g.SetView(s.Header, 0, i*hOffset, width-1, (i+1)*hOffset-1)
+		if err != nil {
+			if err != gocui.ErrUnknownView {
+				return err
+			}
+			v.Title = s.Header
+			v.Autoscroll = true
+		}
+
+	}
+
+	return nil
 
 }
 
@@ -125,7 +164,7 @@ func NewDisplay(cencel context.CancelFunc) (display *Display, err error) {
 func (d *Display) Close() {
 
 	if !d.closed {
-		termbox.Close()
+		d.gui.Close()
 		d.closed = true
 	}
 
@@ -136,15 +175,13 @@ func (d *Display) AddSection(header string) *Section {
 	d.mutex.Lock()
 	defer d.mutex.Unlock()
 
-	s := &Section{
-		Header:  header,
-		display: d,
-	}
+	s := newSection(d, header)
 	d.sections = append(d.sections, s)
 	sort.Slice(d.sections, func(i int, j int) bool {
 		return d.sections[i].Header < d.sections[j].Header
 	})
 
+	d.gui.Execute(d.Layout)
 	return s
 }
 
@@ -161,65 +198,13 @@ func (d *Display) DeleteSection(sec *Section) {
 		}
 	}
 
-}
-
-// Refresh refreshes this display.
-func (d *Display) Refresh() error {
-
-	if len(d.sections) == 0 {
-		return nil
-	}
-
-	d.mutex.Lock()
-	defer d.mutex.Unlock()
-	if d.closed {
-		return fmt.Errorf("Display has been closed already")
-	}
-
-	hOffset := d.Height / len(d.sections)
-	hExtra := d.Height - hOffset*len(d.sections)
-
-	termbox.Clear(termbox.ColorDefault, termbox.ColorDefault)
-	for i, s := range d.sections {
-
-		// Print header.
-		x := 0
-		for _, c := range []rune("─ ") {
-			termbox.SetCell(x, i*hOffset, c, d.HeaderForeground, d.HeaderBackground)
-			x++
-		}
-		if s == nil {
-			panic(fmt.Sprint(d.sections))
-		}
-		for _, c := range []rune(s.Header) {
-			termbox.SetCell(x, i*hOffset, c, d.HeaderForeground|termbox.AttrBold, d.HeaderBackground)
-			x++
-		}
-		for _, c := range []rune(fmt.Sprintf(" %v", strings.Repeat("─", d.Width-x-1))) {
-			termbox.SetCell(x, i*hOffset, c, d.HeaderForeground, d.HeaderBackground)
-			x++
-		}
-
-		// Print body.
-		if i < len(d.sections)-1 && len(s.Body) > hOffset-1 {
-			s.Body = s.Body[len(s.Body)-hOffset+1:]
-		} else if len(s.Body) > hOffset+hExtra-1 {
-			s.Body = s.Body[len(s.Body)-hOffset-hExtra+1:]
-		}
-		for u, line := range s.Body {
-			for v, c := range []rune(line) {
-				termbox.SetCell(v, u+i*hOffset+1, c, termbox.ColorDefault, termbox.ColorDefault)
-			}
-		}
-
-	}
-	return termbox.Flush()
+	d.gui.Execute(func(g *gocui.Gui) error {
+		return g.DeleteView(sec.Header)
+	})
 
 }
 
 // Wait blocks until the given context will be canceled.
 func (d *Display) Wait() error {
-
 	return <-d.done
-
 }
